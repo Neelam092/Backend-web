2c792eb30dafeb8b5391311353a71eceed7122a6
Neelam Rana <63229573+Neelam092@users.noreply.github.com>
2021-04-26 08:10:45 +0530

    Add files via upload

--- backend-web/01.node_js_basics/app.ts
+++ backend-web/01.node_js_basics/app.ts
@@ -1,1 +1,16 @@
+let courseName:string = 'Backend Web development'
+console.log(courseName)
 
+let printNumber =(start : number, end:number):void=>{
+    let temp:string=''
+    if(start<end){
+        for(let i:number=start; i<=end; i++){
+            temp+=`${i} `
+        }
+        console.log(temp)
+    }
+    else{
+        console.log('Invalid numbers')
+    }
+}
+printNumber(5,20)
